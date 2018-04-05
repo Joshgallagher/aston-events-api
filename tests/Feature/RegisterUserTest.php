b881f1e7ce9ecf1c75f2d1fbed93baf3b17c3ff4
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-05 22:08:40 +0100

    Added registration tests - all tests pass

---
+++ tests/Feature/RegisterUserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\ApiTestCase;
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegisterUserTest extends ApiTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_register()
+    {
+        $user = make('User');
+
+        $this->postJson('api/v1/register', array_merge($user->toArray(), ['password' => 'secret']))
+            ->assertStatus(Response::HTTP_CREATED);
+
+        $this->assertDatabaseHas('users', $user->toArray());
+    }
+
+    /** @test */
+    public function all_fields_are_required_to_register()
+    {
+        $user = make('User');
+
+        $this->postJson('api/v1/register', $user->toArray())
+            ->assertJsonFragment([
+                'password' => [
+                    'The password field is required.',
+                ],
+            ])
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /** @test */
+    public function a_user_recieves_a_valid_token_after_registration()
+    {
+        $user = make('User');
+
+        $response = $this->postJson('api/v1/register', array_merge($user->toArray(), ['password' => 'secret']))
+            ->assertStatus(Response::HTTP_CREATED);
+
+        $headers = array_merge($this->getHeaders(), ['Authorization' => $response->getData()->meta->access_token]);
+
+        $this->getJson('api/v1/user', $headers)
+            ->assertJsonFragment([
+                'name' => $user->name,
+            ])
+            ->assertStatus(Response::HTTP_OK);
+    }
+}
