ac97fde46817677f05d9e014fb5dc250a23fb83d
Joshua Gallagher <joshgallagher24@gmail.com>
2018-04-07 03:38:09 +0100

    wip

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -3,22 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class FavoriteController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created Favorite in storage.
      *
      * @param \App\Models\Event $event
      *
@@ -29,39 +19,5 @@ class FavoriteController extends Controller
         $event->favorite();
 
         return response(null, Response::HTTP_NO_CONTENT);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int                      $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
     }
 }
